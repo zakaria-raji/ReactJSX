bf8f6468c5156d35ed0774694d0e52252bf7b27e
zkr <zkr@zkrs-MacBook-Pro.local>
2021-08-13 23:23:01 +0200

    This is a new commit for what I originally planned to be amended

---
+++ src/TPs/11. React Components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Heading(){
+    return <h1>Hellooo</h1>;   
+}
+
+
+export default Heading;
---
+++ src/TPs/11. React Components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List(){
+    return <ul>
+        <li>Raji</li>
+        <li>Jack</li>
+        <li>Mark</li>
+        </ul>
+}
+
+export default List;
